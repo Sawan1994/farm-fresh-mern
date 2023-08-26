2ab00baf2f80b53ec0db699c8b58f380d37deefe
Sawan Patodia <sawan.patodia@gmail.com>
2023-08-26 18:43:16 +0530

    get all tours and create new tour apis

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
+// core modules
+const fs = require('fs');
+
+// third party modules
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res
-    .status(500)
-    .json({ message: 'Hello from express!!!', app: 'Farm Fresh | MERN' });
+// Data fetching on start of app
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+// ROUTE HANDLERS
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint.');
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  const updatedTours = tours.concat(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    (err) => {
+      if (err) {
+        res.status(500).send({
+          status: 'error',
+          message: err.message,
+        });
+        return;
+      }
+
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
+// PORT LISTENER
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
